33544b4475454a5c6fd18ce1cc7fa1d95b684047
Theodor Vararu <theo@vararu.org>
2024-09-18 17:59:23 +0300

    Get basic auth credentials from environment
    Set up dotenv in the test example file.

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,16 @@
-from playwright.sync_api import Page, expect
+import os
+from dotenv import load_dotenv
+from playwright.sync_api import Browser, expect
 
-def test_homepage_title(page: Page):
-    page.goto("https://test.mavistesting.com")
-    expect(page).to_have_title("Manage vaccinations in schools")
+load_dotenv()
+
+def test_homepage_title(browser: Browser):
+    context = browser.new_context(
+        http_credentials={
+            "username": os.getenv("TEST_USERNAME"),
+            "password": os.getenv("TEST_PASSWORD"),
+        }
+    )
+    new_page = context.new_page()
+    new_page.goto(os.getenv("TEST_URL"))
+    expect(new_page.locator("h1")).to_contain_text("Manage vaccinations in schools")
